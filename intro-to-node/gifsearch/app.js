c3318554506de89848286109830c0942fd3de847
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-03-31 10:21:34 -0700

    [add] Added greetings route

--- intro-to-node/gifsearch/app.js
+++ intro-to-node/gifsearch/app.js
@@ -16,6 +16,11 @@ app.get('/', (req, res) => {
     res.render('hello-gif', { gifUrl })
 });
 
+app.get('/greetings/:name', (req, res) => {
+  const name = req.params.name;
+  res.render('greetings', { name });
+})
+
 // Start Server
 app.listen(3000, () => {
     console.log('Gif Search listening on port localhost:3000!');
